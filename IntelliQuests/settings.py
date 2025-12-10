916b8c299f3cc1d78222a58ce36e4fabcc0c9962
chourase205 <mayurichourase2056@gmail.com>
2025-12-10 20:50:10 +0530

    update

--- IntelliQuests/settings.py
+++ IntelliQuests/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ["127.0.0.1", "intelliquest.up.railway.app"]
+ALLOWED_HOSTS = ["*", "intelliquest.up.railway.app"]
 
 CSRF_TRUSTED_ORIGINS = ["https://intelliquest.up.railway.app"]
 

bcab5a9e7f6a3aa2679e29b87f0cfe82f7d5a215
chourase205 <mayurichourase2056@gmail.com>
2026-01-05 17:10:21 +0530

    Fix DEBUG for production (use PostgreSQL)

--- IntelliQuests/settings.py
+++ IntelliQuests/settings.py
@@ -27,7 +27,8 @@ environ.Env.read_env(env_file)
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG=True
+DEBUG=False
+
 
 
 ALLOWED_HOSTS = ["*", "intelliquests.up.railway.app"]
